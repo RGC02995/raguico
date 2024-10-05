fec1f91887aed3d57099090c2a9b00d5c763787b
RGC02995 <raulgc2995@gmail.com>
2024-10-05 11:42:39 +0200

    add: spaces in description experience

--- src/pages/about-me/AboutMe.jsx
+++ src/pages/about-me/AboutMe.jsx
@@ -1,4 +1,3 @@
-import { NavBar } from "../../components/navbar/NavBar";
 import "./about-me.css";
 
 export const AboutMe = () => {
--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,8 +1,10 @@
+import "./experience.css";
+
 export const Experience = () => {
   return (
     <>
-      <p>
-        {" "}
+      <h1 className="title__name">Experiencia</h1>
+      <p className="paragraph__description">
         Desde bien pequeño siempre me ha gustado el mundo informático y la
         tecnología en si, al terminar bachillerato me decidí por probar el mundo
         de la animación 3D, pero como aún era un alma joven, tenía dudas sobre
@@ -12,20 +14,21 @@ export const Experience = () => {
         nunca con ganas ni la motivación para seguir haciendolo, no me llenaba
         lo que hacía, así pues llego el punto en el que dije, quiero trabajar el
         resto de mi vida de aquello que me apasiona, así pues, decidí enfocarme
-        en mi mundo, en el mundo de los ordenadores, en el especial de la
-        programación, no sabía como empezar así que pedi consejo de un amigo el
+        en mi mundo, en el mundo de los ordenadores, en el especial el de la
+        programación, no sabía como empezar, así que, pedí consejo a un amigo el
         cual se dedicaba a este mundo, me enseño cosas básicas y empecé con
-        algún que otro curso de Fernando Herrera, lo hacía muy sencillo, cuando
-        cogi, un poco de bases en HTML, CSS y JavaScript decidí estar una
+        algún que otro curso de Fernando Herrera, lo hacía muy sencillo.
+        <br />
+        <br />
+        Cuando cogi un poco de bases en HTML, CSS y JavaScript decidí estar una
         temporada practicando por mi mismo, entendiendo sobretodo, al final
-        ahorre y me decante por un BootCamp, el cual estuve enfocado durante
-        casi, un año, ahí descubri que me apasiona más el mundo del FrontEnd,
-        quizá porque no le dedicamos tanto tiempo al BackEnd, aún así ambos
-        mundos me apasionan, y quiero seguir aprendiendo más cada día, ahora se
-        que me quiero dedicar a esto y por lo tanto estoy a punto de cursar este
-        2025 un Grado Superior en Desarrollo de Aplicaciones Web, poder
-        conseguir unas practicas, y dedicarme al fin al mundo de la programación
-        que tanto me apasiona.{" "}
+        ahorré y me decanté por un BootCamp, el cual estuve enfocado durante
+        casi un año, ahí descubri que me apasiona más el mundo del FrontEnd, aún
+        así ambos mundos me apasionan, y quiero seguir aprendiendo más cada día,
+        ahora se que me quiero dedicar a esto y por lo tanto estoy a punto de
+        cursar este 2024 un Grado Superior en Desarrollo de Aplicaciones Web,
+        poder conseguir unas prácticas y algo de experiencia laboral, y
+        dedicarme al fin al mundo de la programación que tanto me apasiona.{" "}
       </p>
     </>
   );
